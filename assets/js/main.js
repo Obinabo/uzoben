8dadb0c5e65b21375734525d4306fb98683dbbe6
Wally <walterobinabo@gmail.com>
2022-11-03 01:19:35 +0100

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+    $('.mobitems').hide();
+
+    $('#banner .right').owlCarousel({
+        items : 1,
+        slideSpeed: 300,
+        singleItem : true, 
+        autoPlay: true,
+    });
+    $('.night img').click(function(){
+        $('body').toggleClass('body2');
+    })
+    window.onload = function(){
+        $('#preloader').hide();
+    }
+
+    $('.mobile').click(function(){
+        var $mobitem = $(this).next('.mobitems');
+        if($mobitem.is(':hidden')){
+            $mobitem.slideDown(500);
+            $('.mobile').html('<i class="fa fa-xmark"></i>');
+        }else{
+            $mobitem.slideUp(500);
+            $('.mobile').html('<i class="fa fa-bars"></i>');
+        }
+    });
+})
